b1d345e350f8e3955c317aaf8ef3152477a5f31b
Don <Don.rhodes@icloud.com>
2025-11-14 11:37:44 -0500

    start

---
+++ DND.js
@@ -0,0 +1,483 @@
+const Warpath = (() => {
+    const version = '2025.11.14';
+    if (!state.Strahd) {state.Strahd = {}};
+
+
+    let outputCard = {title: "",subtitle: "",side: "",body: [],buttons: [],};
+
+    const playerCodes = {
+        "Don": "2520699",
+        "DonAlt": "5097409",
+        "Ted": "6951960",
+        "Vic": "4892",
+        "Ian": "4219310",
+    }
+
+    const PlayerIDs = () => {
+        let players = Object.keys(playerCodes);
+        for (let i=0;i<players.length;i++) {
+            let roll20ID = playerCodes[players[i]];
+            let playerObj = findObjs({_type:'player',_d20userid: roll20ID})[0];
+            if (playerObj) {
+                PlayerInfo[playerObj.get("id")] = players[i];
+            }
+        }
+    }
+
+    const simpleObj = (o) => {
+        let p = JSON.parse(JSON.stringify(o));
+        return p;
+    };
+
+    const getCleanImgSrc = (imgsrc) => {
+        let parts = imgsrc.match(/(.*\/images\/.*)(thumb|med|original|max)([^?]*)(\?[^?]+)?$/);
+        if(parts) {
+            return parts[1]+'thumb'+parts[3]+(parts[4]?parts[4]:`?${Math.round(Math.random()*9999999)}`);
+        }
+        return;
+    };
+
+    const tokenImage = (img) => {
+        //modifies imgsrc to fit api's requirement for token
+        img = getCleanImgSrc(img);
+        img = img.replace("%3A", ":");
+        img = img.replace("%3F", "?");
+        img = img.replace("med", "thumb");
+        return img;
+    };
+
+    const DeepCopy = (variable) => {
+        variable = JSON.parse(JSON.stringify(variable))
+        return variable;
+    };
+
+    const PlaySound = (name) => {
+        let sound = findObjs({type: "jukeboxtrack", title: name})[0];
+        if (sound) {
+            sound.set({playing: true,softstop:false});
+        }
+    };
+
+    const FX = (fxname,model1,model2) => {
+        //model2 is target, model1 is shooter
+        //if its an area effect, model1 isnt used
+        if (fxname.includes("System")) {
+            //system fx
+            fxname = fxname.replace("System-","");
+            if (fxname.includes("Blast")) {
+                fxname = fxname.replace("Blast-","");
+                spawnFx(model2.token.get("left"),model2.token.get("top"), fxname);
+            } else {
+                spawnFxBetweenPoints(new Point(model1.token.get("left"),model1.token.get("top")), new Point(model2.token.get("left"),model2.token.get("top")), fxname);
+            }
+        } else {
+            let fxType =  findObjs({type: "custfx", name: fxname})[0];
+            if (fxType) {
+                spawnFxBetweenPoints(new Point(model1.token.get("left"),model1.token.get("top")), new Point(model2.token.get("left"),model2.token.get("top")), fxType.id);
+            }
+        }
+    }
+
+    const pointInPolygon = (point,vertices) => {
+        //evaluate if point is in the polygon
+        px = point.x
+        py = point.y
+        collision = false
+        len = vertices.length - 1
+        for (let c=0;c<len;c++) {
+            vc = vertices[c];
+            vn = vertices[c+1]
+            if (((vc.y >= py && vn.y < py) || (vc.y < py && vn.y >= py)) && (px < (vn.x-vc.x)*(py-vc.y)/(vn.y-vc.y)+vc.x)) {
+                collision = !collision
+            }
+        }
+        return collision
+    }
+
+    const translatePoly = (poly) => {
+        //translate points in a pathv2 polygon to map points
+        let vertices = [];
+        let points = JSON.parse(poly.get("points"));
+        let centre = new Point(poly.get("x"), poly.get("y"));
+        //covert path points from relative coords to actual map coords
+        //define 'bounding box;
+        let minX = Infinity,minY = Infinity, maxX = 0, maxY = 0;
+        _.each(points,pt => {
+            minX = Math.min(pt[0],minX);
+            minY = Math.min(pt[1],minY);
+            maxX = Math.max(pt[0],maxX);
+            maxY = Math.max(pt[1],maxY);
+        })
+        //translate each point back based on centre of box
+        let halfW = (maxX - minX)/2 + minX;
+        let halfH = (maxY - minY)/2 + minY
+        let zeroX = centre.x - halfW;
+        let zeroY = centre.y - halfH;
+        _.each(points,pt => {
+            let x = Math.round(pt[0] + zeroX);
+            let y = Math.round(pt[1] + zeroY);
+            vertices.push(new Point(x,y));
+        })
+        return vertices;
+    }
+
+
+
+    class Point {
+        constructor(x,y) {
+            this.x = x;
+            this.y = y;
+        };
+        toOffset() {
+            let cube = this.toCube();
+            let offset = cube.toOffset();
+            return offset;
+        };
+        toCube() {
+            let x = this.x - HexInfo.pixelStart.x;
+            let y = this.y - HexInfo.pixelStart.y;
+            let q,r;
+            if (pageInfo.type === "hex") {
+                q = (M.b0 * x + M.b1 * y) / HexInfo.size;
+                r = (M.b3 * y) / HexInfo.size;
+            } else if (pageInfo.type === "hexr") {
+                q = (M.b3 * x) / HexInfo.size;
+                r = (M.b1 * x + M.b0 * y) / HexInfo.size;
+            }
+            let cube = new Cube(q,r,-q-r).round();
+            return cube;
+        };
+        distance(b) {
+            return Math.sqrt(((this.x - b.x) * (this.x - b.x)) + ((this.y - b.y) * (this.y - b.y)));
+        }
+        label() {
+            return this.toCube().label();
+        }
+    }
+
+
+    const Angle = (theta) => {
+        while (theta < 0) {
+            theta += 360;
+        }
+        while (theta >= 360) {
+            theta -= 360;
+        }
+        return theta
+    }   
+
+
+    const ButtonInfo = (phrase,action,inline) => {
+        //inline - has to be true in any buttons to have them in same line -  starting one to ending one
+        if (!inline) {inline = false};
+        let info = {
+            phrase: phrase,
+            action: action,
+            inline: inline,
+        }
+        outputCard.buttons.push(info);
+    };
+
+    const SetupCard = (title,subtitle,side) => {
+        outputCard.title = title;
+        outputCard.subtitle = subtitle;
+        outputCard.side = side;
+        outputCard.body = [];
+        outputCard.buttons = [];
+        outputCard.inline = [];
+    };
+
+    const DisplayDice = (roll,tablename,size) => {
+        roll = roll.toString();
+        let table = findObjs({type:'rollabletable', name: tablename})[0];
+        if (!table) {
+            table = findObjs({type:'rollabletable', name: "Neutral"})[0];
+        }
+        let obj = findObjs({type:'tableitem', _rollabletableid: table.id, name: roll })[0];        
+        let avatar = obj.get('avatar');
+        let out = "<img width = "+ size + " height = " + size + " src=" + avatar + "></img>";
+        return out;
+    };
+
+    const PrintCard = (id) => {
+        let output = "";
+        if (id) {
+            let playerObj = findObjs({type: 'player',id: id})[0];
+            let who = playerObj.get("displayname");
+            output += `/w "${who}"`;
+        } else {
+            output += "/desc ";
+        }
+
+        if (!outputCard.side || !Factions[outputCard.side]) {
+            outputCard.side = "Neutral";
+        }
+
+        //start of card
+        output += `<div style="display: table; border: ` + Factions[outputCard.side].borderStyle + " " + Factions[outputCard.side].borderColour + `; `;
+        output += `background-color: #EEEEEE; width: 100%; text-align: center; `;
+        output += `border-radius: 1px; border-collapse: separate; box-shadow: 5px 3px 3px 0px #aaa;;`;
+        output += `"><div style="display: table-header-group; `;
+        output += `background-color: ` + Factions[outputCard.side].backgroundColour + `; `;
+        output += `background-image: url(` + Factions[outputCard.side].image + `), url(` + Factions[outputCard.side].image + `); `;
+        output += `background-position: left,right; background-repeat: no-repeat, no-repeat; background-size: contain, contain; align: center,center; `;
+        output += `border-bottom: 2px solid #444444; "><div style="display: table-row;"><div style="display: table-cell; padding: 2px 2px; text-align: center;"><span style="`;
+        output += `font-family: ` + Factions[outputCard.side].titlefont + `; `;
+        output += `font-style: normal; `;
+
+        let titlefontsize = "1.4em";
+        if (outputCard.title.length > 12) {
+            titlefontsize = "1em";
+        }
+
+        output += `font-size: ` + titlefontsize + `; `;
+        output += `line-height: 1.2em; font-weight: strong; `;
+        output += `color: ` + Factions[outputCard.side].fontColour + `; `;
+        output += `text-shadow: none; `;
+        output += `">`+ outputCard.title + `</span><br /><span style="`;
+        output += `font-family: Arial; font-variant: normal; font-size: 13px; font-style: normal; font-weight: bold; `;
+        output += `color: ` +  Factions[outputCard.side].fontColour + `; `;
+        output += `">` + outputCard.subtitle + `</span></div></div></div>`;
+
+        //body of card
+        output += `<div style="display: table-row-group; ">`;
+
+        let inline = 0;
+
+        for (let i=0;i<outputCard.body.length;i++) {
+            let out = "";
+            let line = outputCard.body[i];
+            if (!line || line === "") {continue};
+            if (line.includes("[INLINE")) {
+                let end = line.indexOf("]");
+                let substring = line.substring(0,end+1);
+                let num = substring.replace(/[^\d]/g,"");
+                if (!num) {num = 1};
+                line = line.replace(substring,"");
+                out += `<div style="display: table-row; background: #FFFFFF;; `;
+                out += `"><div style="display: table-cell; padding: 0px 0px; font-family: Arial; font-style: normal; font-weight: normal; font-size: 14px; `;
+                out += `"><span style="line-height: normal; color: #000000; `;
+                out += `"> <div style='text-align: center; display:block;'>`;
+                out += line + " ";
+
+                for (let q=0;q<num;q++) {
+                    let info = outputCard.inline[inline];
+                    out += `<a style ="background-color: ` + Factions[outputCard.side].backgroundColour + `; padding: 5px;`
+                    out += `color: ` + Factions[outputCard.side].fontColour + `; text-align: center; vertical-align: middle; border-radius: 5px;`;
+                    out += `border-color: ` + Factions[outputCard.side].borderColour + `; font-family: Tahoma; font-size: x-small; `;
+                    out += `"href = "` + info.action + `">` + info.phrase + `</a>`;
+                    inline++;                    
+                }
+                out += `</div></span></div></div>`;
+            } else {
+                line = line.replace(/\[hr(.*?)\]/gi, '<hr style="width:95%; align:center; margin:0px 0px 5px 5px; border-top:2px solid $1;">');
+                line = line.replace(/\[\#([A-Fa-f0-9]{3}|[A-Fa-f0-9]{6})\](.*?)\[\/[\#]\]/g, "<span style='color: #$1;'>$2</span>"); // [#xxx] or [#xxxx]...[/#] for color codes. xxx is a 3-digit hex code
+                line = line.replace(/\[[Uu]\](.*?)\[\/[Uu]\]/g, "<u>$1</u>"); // [U]...[/u] for underline
+                line = line.replace(/\[[Bb]\](.*?)\[\/[Bb]\]/g, "<b>$1</b>"); // [B]...[/B] for bolding
+                line = line.replace(/\[[Ii]\](.*?)\[\/[Ii]\]/g, "<i>$1</i>"); // [I]...[/I] for italics
+                let lineBack,fontcolour;
+                if (line.includes("[F]")) {
+                    let ind1 = line.indexOf("[F]") + 3;
+                    let ind2 = line.indexOf("[/f]");
+                    let fac = line.substring(ind1,ind2);
+                    if (Factions[fac]) {
+                        lineBack = Factions[fac].backgroundColour;
+                        fontcolour = Factions[fac].fontColour;
+                    }
+                    line = line.replace("[F]" + fac + "[/f]","");
+
+                } else {
+                    lineBack = (i % 2 === 0) ? "#D3D3D3": "#EEEEEE";
+                    fontcolour = "#000000";
+                }
+                out += `<div style="display: table-row; background: ` + lineBack + `;; `;
+                out += `"><div style="display: table-cell; padding: 0px 0px; font-family: Arial; font-style: normal; font-weight: normal; font-size: 14px; `;
+                out += `"><span style="line-height: normal; color:` + fontcolour + `; `;
+                out += `"> <div style='text-align: center; display:block;'>`;
+                out += line + `</div></span></div></div>`;                
+            }
+            output += out;
+        }
+
+        //buttons
+        if (outputCard.buttons.length > 0) {
+            for (let i=0;i<outputCard.buttons.length;i++) {
+                let info = outputCard.buttons[i];
+                let inline = info.inline;
+                if (i>0 && inline === false) {
+                    output += '<hr style="width:95%; align:center; margin:0px 0px 5px 5px; border-top:2px solid $1;">';
+                }
+                let out = "";
+                let borderColour = Factions[outputCard.side].borderColour;
+                
+                if (inline === false || i===0) {
+                    out += `<div style="display: table-row; background: #FFFFFF;; ">`;
+                    out += `<div style="display: table-cell; padding: 0px 0px; font-family: Arial; font-style: normal; font-weight: normal; font-size: 14px; `;
+                    out += `"><span style="line-height: normal; color: #000000; `;
+                    out += `"> <div style='text-align: center; display:block;'>`;
+                }
+                if (inline === true) {
+                    out += '<span>     </span>';
+                }
+                out += `<a style ="background-color: ` + Factions[outputCard.side].backgroundColour + `; padding: 5px;`
+                out += `color: ` + Factions[outputCard.side].fontColour + `; text-align: center; vertical-align: middle; border-radius: 5px;`;
+                out += `border-color: ` + borderColour + `; font-family: Tahoma; font-size: x-small; `;
+                out += `"href = "` + info.action + `">` + info.phrase + `</a>`
+                
+                if (inline === false || i === (outputCard.buttons.length - 1)) {
+                    out += `</div></span></div></div>`;
+                }
+                output += out;
+            }
+
+        }
+
+        output += `</div></div><br />`;
+        sendChat("",output);
+        outputCard = {title: "",subtitle: "",side: "",body: [],buttons: [],};
+    }
+
+    //related to building hex map
+    const LoadPage = () => {
+        //build Page Info and flesh out Hex Info
+        pageInfo.page = getObj('page', Campaign().get("playerpageid"));
+        pageInfo.name = pageInfo.page.get("name");
+        pageInfo.scale = pageInfo.page.get("snapping_increment");
+        pageInfo.width = pageInfo.page.get("width") * 70;
+        pageInfo.height = pageInfo.page.get("height") * 70;
+        pageInfo.type = pageInfo.page.get("grid_type");
+
+    }
+
+
+
+    const RollDice = (msg) => {
+        PlaySound("Dice");
+        let roll = randomInteger(8);
+        let playerID = msg.playerid;
+        let id,model,player;
+        if (msg.selected) {
+            id = msg.selected[0]._id;
+        }
+        let faction = "Neutral";
+
+        if (!id && !playerID) {
+            log("Back")
+            return;
+        }
+        if (id) {
+            model = ModelArray[id];
+            if (model) {
+                faction = model.faction;
+                player = model.player;
+            }
+        }
+        if ((!id || !model) && playerID) {
+            faction = state.Warpath.players[playerID];
+            player = (state.Warpath.factions[0] === faction) ? 0:1;
+        }
+
+        if (!state.Warpath.players[playerID] || state.Warpath.players[playerID] === undefined) {
+            if (faction !== "Neutral") {    
+                state.Warpath.players[playerID] = faction;
+            } else {
+                sendChat("","Click on one of your tokens then select Roll again");
+                return;
+            }
+        } 
+        let res = "/direct " + DisplayDice(roll,faction,40);
+        sendChat("player|" + playerID,res);
+    }
+
+
+    //line line collision where line1 is pt1 and 2, line2 is pt 3 and 4
+    const lineLine = (pt1,pt2,pt3,pt4) => {
+        //calculate the direction of the lines
+        uA = ( ((pt4.x-pt3.x)*(pt1.y-pt3.y)) - ((pt4.y-pt3.y)*(pt1.x-pt3.x)) ) / ( ((pt4.y-pt3.y)*(pt2.x-pt1.x)) - ((pt4.x-pt3.x)*(pt2.y-pt1.y)) );
+        uB = ( ((pt2.x-pt1.x)*(pt1.y-pt3.y)) - ((pt2.y-pt1.y)*(pt1.x-pt3.x)) ) / ( ((pt4.y-pt3.y)*(pt2.x-pt1.x)) - ((pt4.x-pt3.x)*(pt2.y-pt1.y)) );
+        if (uA >= 0 && uA <= 1 && uB >= 0 && uB <= 1) {
+            intersection = {
+                x: (pt1.x + (uA * (pt2.x-pt1.x))),
+                y: (pt1.y + (uA * (pt2.y-pt1.y)))
+            }
+            return intersection;
+        }
+        return;
+    }
+
+
+    const TargetAngle = (shooter,target) => {
+        let shooterHex = HexMap[shooter.hexLabel];
+        let targetHex = HexMap[target.hexLabel];
+        //angle from shooter's hex to target's hex
+        let phi = Angle(shooterHex.cube.angle(targetHex.cube));
+        let theta = Angle(shooter.token.get("rotation"));
+        let gamma = Angle(phi - theta);
+        return gamma;
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    const handleInput = (msg) => {
+        if (msg.type !== "api") {
+            return;
+        }
+        let args = msg.content.split(";");
+        log(args);
+    
+        switch(args[0]) {
+            case '!Dump':
+                log("State");
+                log(state.Warpath);
+            case '!ClearState':
+                ClearState(msg);
+                break;
+            case '!Smite':
+                Smite(msg);
+                break;
+
+
+        }
+    };
+
+
+
+
+    const registerEventHandlers = () => {
+        on('chat:message', handleInput);
+    
+    };
+    on('ready', () => {
+        log("===> CoS <===");
+        log("===> Software Version: " + version + " <===")
+        PlayerIDs();
+        registerEventHandlers();
+        sendChat("","API Ready")
+        log("On Ready Done")
+    });
+    return {
+        // Public interface here
+    };
+
+
+
+
+
+
+})();
+
+
