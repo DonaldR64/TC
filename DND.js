dde681b5826587b62eb5eb5541f97690914749c5
Don <Don.rhodes@icloud.com>
2025-11-18 23:48:21 -0500

    a

--- DND.js
+++ DND.js
@@ -311,7 +311,64 @@ const lerp = (start, end, t) => {
     return start * (1.0 - t) + t * end;
 }
 
-const TestLine = (msg) => {
+
+const EndLine = (start,end,length) => {
+    //produces a line representing end of Cone of length x, using a start point (caster) and end pt (target)
+    let distance = ((length/2) / pageInfo.scaleNum) * 70;
+    let p0 = MapArray[start].centre;
+    let p1 = MapArray[end].centre;
+    //for cones, width = length
+    let m0 = (p1.y - p0.y)/(p1.x - p0.x);
+    let p2,p3;
+
+
+    if (m0 === 0) {
+        //line is horizontal, so perp is vertical
+        p2 = new Point(p0.x,p0.y + distance);
+        p3 = new Point(p0.x,p0.y - distance);
+    } else {
+        let m1 = -1/m0;
+        let b1 = p1.y - (m1 * p1.x);
+        p2 = findPointOnLine(p1,m1,b1,distance,1);
+        p3 = findPointOnLine(p1,m1,b1,distance,-1);
+    }
+
+    let index2 = p2.toIndex();
+    let index3 = p3.toIndex();
+
+    let line = Line(index2,index3);
+
+    return line;
+}
+
+const findPointOnLine = (point,m,b,distance,direction) => {
+    let magnitude = Math.sqrt(1+m*m);
+    let deltaX = direction * (distance / magnitude);
+    let deltaY = direction * (m * distance / magnitude);
+    let pt = new Point(point.x + deltaX,point.y + deltaY);
+    return pt;
+}
+
+const Cone = (start,end,length) => {
+    //length is in feet
+    let sqL = length / pageInfo.scaleNum;
+    let endLine = EndLine(start,end,length);
+    let areaIndexes = [];
+    for (let i=0;i<endLine.length;i++) {
+        let line = Line(start,endLine(i));
+        for (j=0;j<line.length;j++) {
+            let index = line[j];
+            let dist = MapArray[start].distance(MapArray[index]);
+            if (dist <= sqL) {
+                areaIndexes.push(index);
+            }
+        }
+    }
+    areaIndexes = [...new Set(areaIndexes)];
+    return areaIndexes;
+}
+
+const TestCone = (msg) => {
     let Tag = msg.content.split(";");
     let id1 = Tag[1];
     let id2 = Tag[2];
@@ -319,9 +376,9 @@ const TestLine = (msg) => {
     let model2 = ModelArray[id2];
     let index1 = model1.squares[0];
     let index2 = model2.squares[0];
-    let line = Line(index1,index2);
-    SetupCard("Test Line","","NPC");
-    _.each(line,pt => {
+    let cone = Cone(index1,index2,15);
+    SetupCard("Test Cone","","NPC");
+    _.each(cone,pt => {
         outputCard.body.push(pt);
     })
     PrintCard();
@@ -2405,8 +2462,8 @@ log(model.name)
             case '!TokenInfo':
                 TokenInfo(msg);
                 break;
-            case '!TestLine':
-                TestLine(msg);
+            case '!TestCone':
+                TestCone(msg);
                 break;
 
 //Saves
--- snippet.js
+++ snippet.js
@@ -1,15 +1,55 @@
-const ConeEndPts = (start,end,length) => {
+const EndLine = (start,end,length) => {
+    //produces a line representing end of Cone of length x, using a start point (caster) and end pt (target)
+    let distance = ((length/2) / pageInfo.scaleNum) * 70;
     let p0 = MapArray[start].centre;
     let p1 = MapArray[end].centre;
     //for cones, width = length
-    
+    let m0 = (p1.y - p0.y)/(p1.x - p0.x);
+    let p2,p3;
 
 
+    if (m0 === 0) {
+        //line is horizontal, so perp is vertical
+        p2 = new Point(p0.x,p0.y + distance);
+        p3 = new Point(p0.x,p0.y - distance);
+    } else {
+        let m1 = -1/m0;
+        let b1 = p1.y - (m1 * p1.x);
+        p2 = findPointOnLine(p1,m1,b1,distance,1);
+        p3 = findPointOnLine(p1,m1,b1,distance,-1);
+    }
 
+    let index2 = p2.toIndex();
+    let index3 = p3.toIndex();
 
+    let line = Line(index2,index3);
 
+    return line;
+}
 
+const findPointOnLine = (point,m,b,distance,direction) => {
+    let magnitude = Math.sqrt(1+m*m);
+    let deltaX = direction * (distance / magnitude);
+    let deltaY = direction * (m * distance / magnitude);
+    let pt = new Point(point.x + deltaX,point.y + deltaY);
+    return pt;
+}
 
-
-
+const Cone = (start,end,length) => {
+    //length is in feet
+    let sqL = length / pageInfo.scaleNum;
+    let endLine = EndLine(start,end,length);
+    let areaIndexes = [];
+    for (let i=0;i<endLine.length;i++) {
+        let line = Line(start,endLine(i));
+        for (j=0;j<line.length;j++) {
+            let index = line[j];
+            let dist = MapArray[start].distance(MapArray[index]);
+            if (dist <= sqL) {
+                areaIndexes.push(index);
+            }
+        }
+    }
+    areaIndexes = [...new Set(areaIndexes)];
+    return areaIndexes;
 }
